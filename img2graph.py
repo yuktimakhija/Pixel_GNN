07e6dff3c433c7e46223351c8bb8ae3d960ac0a0
yuktimakhija <bb1190067@iitd.ac.in>
2022-04-17 10:10:43 +0530

    i2g

--- img2graph.py
+++ img2graph.py
@@ -1,9 +1,11 @@
 # from graphviz import view
 import numpy as np
 from torch_geometric.data import Data
+import torch_geometric.utils
 import torch
 from skimage.util import view_as_blocks
-import json
+from config import config
+from tqdm import tqdm
 # def loader(dataset):
 # 	# handle datasets 
 # 	# will output dim=3, even for grayscale (0.5 = [0.5,0.5,0.5])
@@ -17,7 +19,7 @@ import json
 # img looks like this: [ [0.5, 0.1, 0.9, 1], [.........], [............]]
 # label = np.load("bcv/label.npy")
 
-config = json.load("config.json")
+# config = json.load(open("config.py"))
 
 def img2graph(img, label=None):
 	n = img.shape[0] # 256
@@ -61,28 +63,49 @@ def img2graph(img, label=None):
 		return -(alpha*abs(x[i] - x[j]) + beta*abs(y[i] - y[j]))
 
 	def addedge(a, b):
-		if a < 0 or a > num_nodes or b < 0 or b > num_nodes:
+		if a < 0 or a >= num_nodes or b < 0 or b >= num_nodes:
 			return
 		edges[0].append(a)
 		edges[1].append(b)
 		if label is not None:
-			edge_weights.append([-(alpha*abs((x[i] - x[j]).mean()) + beta*abs(y[i] - y[j]))])
+			edge_weights.append([-(alpha*abs((x[a] - x[b]).mean()) + beta*abs(y[a] - y[b]))])
 		else:
-			edge_weights.append([-(alpha*abs((x[i] - x[j]).mean()) )])
+			edge_weights.append([-(alpha*abs((x[a] - x[b]).mean()) )])
 		# edges.append([a,b])
+		
 
 	# def addweight():
 
-	for i in range(n**2):
+	for i in tqdm(range(n**2)):
 		for j in range(1,k):
 			# todo: vectorize these calls ?
-			addedge(i, i-j)
-			addedge(i, i-n*j)
-			addedge(i, i+j)
-			addedge(i, i+n*j)
+			addedge(i, i-j) #left
+			addedge(i, i-n*j) #top
+			addedge(i, i+j) #right
+			addedge(i, i+n*j) #bottom
+			for l in range(1,j+1): #diagonal
+				addedge(i, i-n*j -l) #top left
+				addedge(i, i-n*j +l) #top right
+				addedge(i, i+n*j -l) #bottom left
+				addedge(i, i+n*j +l) #bottom right
 
 	edges = torch.tensor(edges, dtype = torch.long)
 	edge_weights = torch.tensor(edge_weights, dtype = torch.float)
 	# if y is not None:
 	return Data(x=x, y=y, edge_index=edges, edge_attr=edge_weights)
 	# else return Data(x=x, edge_index=edges, edge_attr=edge_weights)
+
+def visualise_graph(data):
+	import matplotlib.pyplot as plt
+	import igraph as ig
+	adj = torch_geometric.utils.to_dense_adj(data['edge_index'], edge_attr=data['edge_attr'])
+	g = ig.Graph.Weighted_Adjacency(adj.tolist()[0])
+	l = g.layout('lgl')
+	ig.plot(g, layout=l)
+	# plt.show()
+
+if __name__ == "__main__":
+	img = np.load("sample_bcv/img.npy")
+	label = np.load("sample_bcv/label.npy")
+	data = img2graph(img,label)
+	visualise_graph(data)
