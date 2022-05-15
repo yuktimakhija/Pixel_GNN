2d185eefbcd7711cbe8904907705185298dc8673
Plutonium-239 <plutonium.239.811@gmail.com>
2022-05-16 04:32:24 +0530

    updates

--- semi-supervised/dataloader.py
+++ semi-supervised/dataloader.py
@@ -179,10 +179,10 @@ class GeneralDataLoader(Dataset):
 		assert len(support_label_list) == self.shot and len(support_image_list) == self.shot                    
 		assert len(unsup_image_list) == self.unsup
 		
-		return support_image_list, support_label_list, unsup_image_list, [p(img)]
+		# return support_image_list, support_label_list, unsup_image_list, [p(img)]
 		# q_graph = img2graph(img, label)
 		q_index, sup_index, sup_graph, unsup_graph, task_graph =\
-			 support_graph_matrix(support_image_list, support_label_list, unsup_image_list, [img])
+			 support_graph_matrix(support_image_list, support_label_list, unsup_image_list, [p(img)])
 		if self.mode == 'train':
 			return q_index, sup_index, sup_graph, unsup_graph, task_graph, torch.tensor(label.reshape(-1))
 		else:
